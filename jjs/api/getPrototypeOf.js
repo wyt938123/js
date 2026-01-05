2c43d59871b690f6c1606490c57b1cc36ae8199b
wyt938123 <15827660592qq.com>
2026-01-05 18:09:59 +0800

    yidian

---
+++ jjs/api/getPrototypeOf.js
@@ -0,0 +1,3 @@
+获取实例对象的原型
+// 比如 [] 的原型是 Array.prototype
+// [].getPrototypeOf = Array.prototype
---
+++ 静态.js
@@ -0,0 +1,4 @@
+class Static {
+     static staticProperty = 'I am a static property';
+}
+console.log(Static.staticProperty); // Output: I am a static property
